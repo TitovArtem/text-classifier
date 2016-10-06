74644eb7631049dceb94c073f5b45cddeff17a76
Titov Artem <pbi62007@yandex.ru>
2016-10-06 17:18:43 +0300

    Change <BaseEstimator> for working with object public attributes.

--- textclassifier/core/base.py
+++ textclassifier/core/base.py
@@ -5,42 +5,49 @@ from abc import abstractmethod, ABCMeta
 class BaseEstimator(object):
     """ The base class for all estimators. """
 
-    def __init__(self, **params):
-        self._params = params if params else {}
+    def get_params(self):
+        """ Return the dictionary of public parameters for this estimator. """
+        return dict((k, v) for k, v in self.__dict__.items()
+                    if not k.startswith("_"))
 
-    @property
-    def params(self):
-        """ Return the dictionary of parameters for this estimator. """
-        return self._params
-
-    @params.setter
-    def params(self, **params):
-        """ Set the parameters for this estimator. """
+    def set_params(self, **params):
+        """ Set the public parameters for this estimator. """
         if not params:
             return
 
         self._check_params(**params)
-        for key, val in params:
-            self._params[key] = copy.copy(val)
+        for key, val in params.items():
+            self.__dict__[key] = copy.deepcopy(val)
 
     def _check_params(self, **params):
         for p in params.keys():
-            if p not in self._params.keys():
-                raise ValueError("Invalid parameter %s for estimator %s." %
+            if p not in self.get_params():
+                raise ValueError("Invalid parameter '%s' for estimator %s." %
                                  (p, self))
+
+    def __repr__(self):
+        return "%s: %s" % (self.__class__.__name__, self.get_params())
 
 
 class UnsupervisedModel(BaseEstimator, metaclass=ABCMeta):
-    """ The base class for models without learning. """
+    """ The base class for models based on unsupervised learning. """
 
     @abstractmethod
     def predict(self, x):
-        """ Predict results for x. """
+        """
+        Predict results for x.
+        :param x: numpy array, test data
+        :return: numpy array, predicted answers
+        """
 
 
 class SupervisedModel(UnsupervisedModel, metaclass=ABCMeta):
-    """ The base class for models with supervised learning. """
+    """ The base class for models based on supervised learning. """
 
     @abstractmethod
     def train(self, x, y):
-        """ Train model. """
+        """
+        Train model.
+        :param x: numpy array, train data
+        :param y: numpy array, target data
+        """
